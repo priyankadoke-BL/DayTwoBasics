335de83a7ed33f6d2162423c64fdc4f1d77fc4b5
Priyankadoke12345 <priyankaadoke@gmail.com>
2021-09-17 23:20:53 +0530

    Initial commit

---
+++ src/ReverseNumMain.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ReverseNumMain {
+    public static void main(String[] args) {
+        System.out.println("Enter a number");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int rev_num = 0;
+        while (num > 0)
+        {
+            rev_num = rev_num * 10 + num % 10;
+            num = num / 10;
+        }
+        System.out.println("Reverse of a number is " + rev_num);
+    }
+}
+
---
+++ src/Reverseformain.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Reverseformain {
+    public static void main(String[] args) {
+        System.out.println("Enter a number");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int rev_num = 0;
+        for(int i =num; i>0 ; i = num){
+            rev_num = rev_num * 10 + num % 10;
+            num = num/10;
+        }
+        System.out.println("Reverse of number is " + rev_num);
+    }
+}
